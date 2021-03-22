4d32b8573bedaa32063c261b39fe5e6b603f6e4d
lievertom <lievertom@gmail.com>
2021-03-22 17:18:40 -0300

    Fix information message

--- load-seed.py
+++ load-seed.py
@@ -2,11 +2,11 @@ from os import system
 
 
 print('Essa operação pode sobrescrever os dados da base.')
-print('Você deseja carregar os dados? Y/n')
+print('Você deseja carregar os dados? (Y/n)')
 
 user_input = input()
 
-if user_input == 'Y':
+if user_input.lower() == 'y':
     system('python manage.py loaddata word_portuguese')
     system('python manage.py loaddata phrase_portuguese')
     system('python manage.py loaddata pronunciation_type')
