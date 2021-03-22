bdcfb13d49257219ec0225a0298bd0ca3044cb5b
Luiz Gustavo <luiz1gustavocampos@gmail.com>
2021-03-21 21:15:40 -0300

    Create fuction to render created template
    Co-authored-by: Andre Lucas <andrelucax@gmail.com>

---
+++ dictionary/views_html.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def add_translate(request):
+    if(request.user.is_superuser):
+        if(request.method == 'GET'):
+            return render(request, 'add_translation.html')
+        elif(request.method == 'POST'):
+            print(request.POST.get('kokama_word'))
+
+        return HttpResponse('Sou adminstrador fom')
+    else:
+        return HttpResponse('APENAS ADMINISTRADORES')
