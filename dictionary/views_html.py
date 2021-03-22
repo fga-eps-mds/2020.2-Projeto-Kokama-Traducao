73c186afbeacf08ce5c2be1df894e790155e2348
Luiz Gustavo <luiz1gustavocampos@gmail.com>
2021-03-22 13:08:52 -0300

    Create dropdown for pronunciation type and save in database
    Co-authored-by: Andre Lucas <andrelucax@gmail.com>

--- dictionary/views_html.py
+++ dictionary/views_html.py
@@ -1,12 +1,31 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import WordKokama, WordPortuguese
+from .models import PhrasePortuguese, PhraseKokama
+
 
 def add_translate(request):
     if(request.user.is_superuser):
         if(request.method == 'GET'):
             return render(request, 'add_translation.html')
         elif(request.method == 'POST'):
-            print(request.POST.get('kokama_word'))
+            portuguese_word = request.POST.get('portuguese_word')
+            kokama_word = request.POST.get('kokama_word')
+            phrase_portuguese = request.POST.get('phrase_portuguese')
+            phrase_kokama = request.POST.get('phrase_kokama')
 
+            wordPortuguese = WordPortuguese(word_portuguese=portuguese_word)
+            wordPortuguese.save()
+            
+            wordKokama = WordKokama(word_kokama=kokama_word)
+            wordKokama.save()
+            
+            phrasePortuguese = PhrasePortuguese(phrase_portuguese=phrase_portuguese)
+            phrasePortuguese.save()
+            
+            phraseKokama = PhraseKokama(phrase_kokama=portuguese_word)
+            phraseKokama.save()
+
+            return HttpResponse('Okay')
     else:
         return HttpResponse('APENAS ADMINISTRADORES')
