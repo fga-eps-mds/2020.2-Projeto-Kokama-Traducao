da73e6fd971fbc48c01c51811e716bd90f30ec9b
luisgaboardi <luis.ggaboardi@gmail.com>
2021-04-05 19:49:23 -0300

    Fix project file organization
    Co-authored-by: Lieverton Santos <lievertom@gmail.com>
    Co-authored-by: Welison Almeida <welison.almeida.923@gmail.com>
    Co-authored-by: nickby2 <machiromonteiro@gmail.com>

--- administration/models.py
+++ administration/models.py
@@ -1,8 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Email(models.Model):
-    email = models.EmailField(max_length= 50)
-
-    def __str__(self):
-        return self.email
