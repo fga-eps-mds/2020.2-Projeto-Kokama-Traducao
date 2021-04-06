c861900bde072899e71f9f8fedcda10ac1a98354
luisgaboardi <luis.ggaboardi@gmail.com>
2021-04-05 21:45:40 -0300

    Create kokama words endpoint
    Co-authored-by: Lucas Rodrigues <machiromonteiro@gmail.com>

--- dictionary/views.py
+++ dictionary/views.py
@@ -1,8 +1,12 @@
 from rest_framework import viewsets, mixins
-from .serializers import WordKokamaSerializer
+from .serializers import WordKokamaSerializer, WordsSerializer
 from .models import WordKokama
 
 
 class KokamaViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     queryset = WordKokama.objects.all()
     serializer_class = WordKokamaSerializer
+
+class WordsViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    queryset = WordKokama.objects.all()
+    serializer_class = WordsSerializer
