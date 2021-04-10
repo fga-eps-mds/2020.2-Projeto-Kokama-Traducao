5734636e670e97bc35c94b80ede8cbc2260009cd
Leonardo Medeiros <leonardomedeiros.6@gmail.com>
2021-04-10 20:47:47 -0300

    Add tests folder structure; Configure Sqlite3 as default development db
    Co-authored-by: Welison Almeida <welison.almeida.923@gmail.com>

--- translate/settings.py
+++ translate/settings.py
@@ -17,18 +17,19 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ENVIRONMENT = config('ENVIRONMENT')
+ENVIRONMENT = config('ENVIRONMENT', default='DEVELOPMENT')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY',
+                    default='lb503%-=26zc*r2imy*4+w$x@wsg@jbp_i8ddt_fo8e-b#4k)@')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = [config('ALLOWED_HOSTS')]
+ALLOWED_HOSTS = [config('ALLOWED_HOSTS', default='*')]
 
 
 # Application definition
@@ -87,16 +88,24 @@ WSGI_APPLICATION = 'translate.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('POSTGRES_DB'),
-        'USER': config('POSTGRES_USER'),
-        'PASSWORD': config('POSTGRES_PASSWORD'),
-        'HOST': 'db',
-        'PORT': config('POSTGRES_PORT'),
+if ENVIRONMENT == 'DEVELOPMENT':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': config('POSTGRES_DB'),
+            'USER': config('POSTGRES_USER'),
+            'PASSWORD': config('POSTGRES_PASSWORD'),
+            'HOST': 'db',
+            'PORT': config('POSTGRES_PORT'),
+        }
+    }
 
 if ENVIRONMENT == 'HOMOLOGATION':
     import dj_database_url
