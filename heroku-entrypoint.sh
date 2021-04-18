9cbb0c3a2b5171772ccc6cb5a298739e054440f8
luisgaboardi <luis.ggaboardi@gmail.com>
2021-04-17 21:44:36 -0300

    Remove unnused command in heroku entrypoint
    Co-authored-by: Andre Lucas <andrelucax@gmail.com>

--- heroku-entrypoint.sh
+++ heroku-entrypoint.sh
@@ -5,5 +5,4 @@ python3 manage.py makemigrations dictionary
 python3 manage.py makemigrations 
 python3 manage.py migrate
 python3 manage.py migrate --run-syncdb
-python3 manage.py collectstatic --no-input
 python3 manage.py runserver 0.0.0.0:$PORT
